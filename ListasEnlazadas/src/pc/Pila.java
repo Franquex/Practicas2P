9661fef85c19052d0eb92c8de48620267e5654d3
Franquex <franquitoturey29@gmail.com>
2025-05-09 10:26:56 -0600

    pc

---
+++ ListasEnlazadas/src/pc/Pila.java
@@ -0,0 +1,17 @@
+package pc;
+
+import lse.Nodo;
+
+public class Pila  extends Cola {
+    @Override
+    public void eliminar() {
+            if (!estaVacia()) {
+                Nodo ultimo = primero;
+                Nodo penultimo = null;
+                while (ultimo.getSiguiente() != null){
+                    penultimo= ultimo;
+                    ultimo = ultimo.getSiguiente();
+            }
+        }
+    }
+}
